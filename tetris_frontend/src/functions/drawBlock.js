ac1758e643bb193e3e7594a2bf2fcb3ee371b54b
Mansook <orgin20@naver.com>
2022-10-21 17:56:52 +0900

    color

--- tetris_frontend/src/functions/drawBlock.js
+++ tetris_frontend/src/functions/drawBlock.js
@@ -1,3 +1,26 @@
+export const blockColor = (blockType) => {
+  let color = 0;
+  blockType.map((arr) => arr.map((v) => (v !== 0 ? (color = v) : color)));
+  switch (color) {
+    case 0:
+      return "white";
+    case 1:
+      return "red";
+    case 2:
+      return "blue";
+    case 3:
+      return "skyblue";
+    case 4:
+      return "orange";
+    case 5:
+      return "green";
+    case 6:
+      return "pink";
+    default:
+      return "white";
+  }
+};
+
 export const drawBlock = (block, ctx) => {
   if (ctx) {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
@@ -5,7 +28,7 @@ export const drawBlock = (block, ctx) => {
     block.type.forEach((row, y) => {
       row.forEach((value, x) => {
         if (value > 0) {
-          ctx.fillStyle = "white";
+          ctx.fillStyle = blockColor(block.type);
           ctx.fillRect(x + block.x, y + block.y, 1, 1);
         }
       });
--- tetris_frontend/src/functions/drawBoard.js
+++ tetris_frontend/src/functions/drawBoard.js
@@ -1,9 +1,29 @@
+const readColor = (v) => {
+  switch (v) {
+    case 0:
+      return "white";
+    case 1:
+      return "red";
+    case 2:
+      return "blue";
+    case 3:
+      return "skyblue";
+    case 4:
+      return "orange";
+    case 5:
+      return "green";
+    case 6:
+      return "pink";
+    default:
+      return "white";
+  }
+};
 export const drawBoard = (matrix, ctx) => {
   if (ctx) {
     matrix.forEach((row, y) => {
       row.forEach((value, x) => {
         if (value > 0) {
-          ctx.fillStyle = "white";
+          ctx.fillStyle = readColor(value);
           ctx.fillRect(x, y, 1, 1);
         }
       });
